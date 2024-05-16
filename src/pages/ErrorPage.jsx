46a722e775971533b8d9cfa3ca596fff8708a5f6
querlemtle <72937427+querlemtle@users.noreply.github.com>
2024-05-16 17:01:29 +0800

    feat: scss common styles

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -4,11 +4,11 @@ export default function ErrorPage() {
   const error = useRouteError();
 
   return (
-    <div className="h-full flex flex-col justify-center items-center">
-      <h1 className="mb-2 text-5xl font-semibold text-primary-900">
+    <div>
+      <h1>
         頁面出錯了...
       </h1>
-      <p className="text-2xl text-secondary-700">錯誤代碼：{error.status}</p>
+      <p>錯誤代碼：{error.status}</p>
     </div>
   );
 }
