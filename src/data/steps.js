b74a92f58ce245794192dcf45aedb79896b748e0
querlemtle <72937427+querlemtle@users.noreply.github.com>
2024-05-20 00:20:17 +0800

    feat: update assets and data

---
+++ src/data/steps.js
@@ -0,0 +1,16 @@
+const steps = [
+  {
+    num: 1,
+    content: "至醫療院所申請醫師開立的診斷證明、照會或醫囑"
+  },
+  {
+    num: 2,
+    content: "透過電話或線上預約看診"
+  },
+  {
+    num: 3,
+    content: "請穿著輕便服裝，並在預約時間前五分鐘抵達"
+  }
+];
+
+export default steps;
