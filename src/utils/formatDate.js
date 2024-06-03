70bec8a29fd77be4f04f99b676412710f48300b9
querlemtle <72937427+querlemtle@users.noreply.github.com>
2024-06-03 14:20:22 +0800

    fix: format date style

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,11 +1,11 @@
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat/DateTimeFormat
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat/DateTimeFormat#style_shortcuts
 // 時區列表：https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 function formatDate(dateStr) {
   const d = new Date(dateStr);
   const formattedDate = new Intl.DateTimeFormat("zh-Hans-TW", {
     year: "numeric",
-    month: "numeric",
-    day: "numeric"
+    month: "2-digit",
+    day: "2-digit"
   }).format(d);
   return formattedDate;
 }
