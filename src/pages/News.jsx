85d73da320e09c215402105cd4cb574621efe823
querlemtle <72937427+querlemtle@users.noreply.github.com>
2024-06-06 15:43:55 +0800

    feat: news page layout

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,22 +1,26 @@
 import news from "@/data/news";
 import NewsCard from "@/components/NewsCard";
 import formatDate from "@/utils/formatDate";
+import FlowSvg from "@/components/FlowSvg";
 
 export default function News() {
   return (
     <main className="main">
-      <h2 className="title title--center">最新消息</h2>
-      <div>
-        {news.map((news) => {
-          return (
-            <NewsCard
-              key={news.date}
-              date={formatDate(news.date)}
-              title={news.title}
-            />
-          );
-        })}
-      </div>
+      <section className="news news--full">
+        <FlowSvg />
+        <h2 className="title title--center">最新消息</h2>
+        <div className="news__grid">
+          {news.map((news) => {
+            return (
+              <NewsCard
+                key={news.date}
+                date={formatDate(news.date)}
+                title={news.title}
+              />
+            );
+          })}
+        </div>
+      </section>
     </main>
   );
 }
